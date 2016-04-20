e3b63f0443c1a9744ace0887f7302fc110d9c600
n1kl <nick4542@yahoo.com>
2016-04-20 14:25:19 +0200

    deprecated entfernt

--- src/de/dis2011/Main.java
+++ src/de/dis2011/Main.java
@@ -1,8 +1,6 @@
 package de.dis2011;
 
-import de.dis2011.data.Makler;
 import de.dis2016.ui.main_ui;
-import de.dis2016.ui.manager_ui;
 
 /**
  * Hauptklasse
@@ -17,7 +15,7 @@ public class Main {
 		
 			main_ui main_ui = new main_ui();
 			main_ui.setSize(230,200);
-			main_ui.show();
+			main_ui.setVisible(true);
 			
 	}
 
